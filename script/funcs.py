d65177bb2aa4df171416c8a14468d179015d5a31
KEYAN527 <43232313+KEYAN527@users.noreply.github.com>
2021-07-12 20:43:29 +0800

    Update funcs.py

--- script/funcs.py
+++ script/funcs.py
@@ -18,7 +18,7 @@ def get_portfolio_nav(group,broker_name,mkt):
     portfolio[mkt+'代码'] = portfolio[mkt+'代码'].apply(lambda x:np.str(x)+'.OF')
     fund_list = list(portfolio[mkt+'代码'])
     
-    data = pd.read_csv('./data/ETF_nav.csv',encoding = 'gbk')
+    data = pd.read_csv('./data/ETF_nav.CSV',encoding = 'gbk')
     data['index'] = pd.to_datetime(data['日期'],format = '%Y-%m-%d')
     data = data.loc[(data['index']>=time_start)&(data['index']<=time_end)&(data['代码'].isin(fund_list)),
                     :]
@@ -26,7 +26,7 @@ def get_portfolio_nav(group,broker_name,mkt):
     nav = pd.DataFrame(nav.reset_index(),index = range(len(nav))).set_index('index')
     
     #对于策略起始日还没有发布的指数基金产品，以基金基准指数净值作为替代
-    index_nav =  pd.read_csv('./data/index_nav.csv',encoding = 'gbk')
+    index_nav =  pd.read_csv('./data/index_nav.CSV',encoding = 'gbk')
     index_nav = index_nav.rename(columns = {'Unnamed: 0':'index'},inplace = False)
     index_nav['index'] = pd.to_datetime(index_nav['index'],format = '%Y-%m-%d')
     for i in nav.columns:
@@ -57,7 +57,7 @@ def get_portfolio_nav(group,broker_name,mkt):
     return nav 
 
 def get_index(nav,index_code = '000905.XSHG'):
-    daily_index = pd.read_csv('./data/bench_index_nav.csv')
+    daily_index = pd.read_csv('./data/bench_index_nav.CSV')
     daily_index['index'] = pd.to_datetime(daily_index['index'],format = '%Y-%m-%d')
     
     daily_index = daily_index.loc[(daily_index['index']>=nav.index.values[0])&(daily_index['index']<=nav.index.values[-1]),
