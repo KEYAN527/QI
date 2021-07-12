c044643d9dbf52dd513fae290adca024b4cb4226
KEYAN527 <43232313+KEYAN527@users.noreply.github.com>
2021-07-12 20:16:28 +0800

    Update funcs.py

--- script/funcs.py
+++ script/funcs.py
@@ -6,68 +6,35 @@ Created on Mon Jun 28 02:20:17 2021
 """
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-from WindPy import *
-w.start()
-
-#作图函数
-import plotly.graph_objects as go
-import plotly.express as px
-import plotly.io as pio
-pio.templates.default = "none"
-
-import jqdatasdk as jq
-# 聚宽账号
-jq.auth('13683364770', '1113Rorschach')
-# 查询是否连接成功
-is_auth = jq.is_auth()
-print(is_auth)
-
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 25 10:25:41 2021
-
-@author: Administrator
-"""
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-from WindPy import *
-w.start()
-
-#作图函数
-import plotly.graph_objects as go
-import plotly.express as px
-import plotly.io as pio
-pio.templates.default = "none"
-
-import jqdatasdk as jq
-# 聚宽账号
-jq.auth('13683364770', '1113Rorschach')
-# 查询是否连接成功
-is_auth = jq.is_auth()
-print(is_auth)
 
 def get_portfolio_nav(group,broker_name,mkt):    
     portfolio = group[(group['券商'] == broker_name)&(group[mkt+'权重']>0)][[mkt + '基金',mkt + '代码',mkt + '权重']]
-    portfolio[mkt+'代码'] = portfolio[mkt+'代码'].apply(lambda x:np.str(x)+'.OF')
     strategy_name = group.loc[group['券商']==broker_name,'策略名称'].values[0]
-    fund_list = list(portfolio[mkt+'代码'])
-
     time_start = np.str(group[group['券商'] == broker_name]['时间'].values[0])[:10]
-    time_end = datetime.now().strftime('%Y-%m-%d')
+    time_end = '2021-07-09'
+    del group
     
-    data = w.wsd(",".join(fund_list), "NAV_adj", time_start, time_end, "")
-    nav = pd.DataFrame(data.Data).T
-    nav.columns = data.Codes
-    nav.index = data.Times
+    portfolio[mkt+'代码'] = portfolio[mkt+'代码'].apply(lambda x:np.str(x)+'.OF')
+    fund_list = list(portfolio[mkt+'代码'])
+    
+    data = pd.read_csv('./data/ETF_nav.csv',encoding = 'gbk')
+    data['index'] = pd.to_datetime(data['日期'],format = '%Y-%m-%d')
+    data = data.loc[(data['index']>=time_start)&(data['index']<=time_end)&(data['代码'].isin(fund_list)),
+                    :]
+    nav = data.pivot_table(index = 'index',columns = '代码',values = '复权净值(元)')
+    nav = pd.DataFrame(nav.reset_index(),index = range(len(nav))).set_index('index')
+    
     #对于策略起始日还没有发布的指数基金产品，以基金基准指数净值作为替代
+    index_nav =  pd.read_csv('./data/index_nav.csv',encoding = 'gbk')
+    index_nav = index_nav.rename(columns = {'Unnamed: 0':'index'},inplace = False)
+    index_nav['index'] = pd.to_datetime(index_nav['index'],format = '%Y-%m-%d')
     for i in nav.columns:
         if nav[i].isnull().values[0] == True:
-            i_index = w.wsd(i,"fund_trackindexcode").Data[0][0]
-            i_indexnav = pd.DataFrame(w.wsd(i_index, "close", time_start, time_end, "").Data[0])
+            i_index = data.loc[data['代码']==i,'标的指数'].values[0]
+            if i_index[0] =='h':
+                i_index = 'H'+i_index[1:]
+            i_indexnav = index_nav.loc[(index_nav['index']>=time_start)&(index_nav['index']<=time_end),i_index]
             i_indexnav = i_indexnav/i_indexnav[:1].values
             nav[i] = i_indexnav.values
     
@@ -84,26 +51,25 @@ def get_portfolio_nav(group,broker_name,mkt):
     port_nav = pd.DataFrame(port_nav,columns = [strategy_name],index = nav.index)
     nav = pd.merge(nav.reset_index(),port_nav.reset_index(),on = ['index'],how = 'outer')
     nav = nav.set_index(['index'])
+    del port_nav
+    del portfolio
     
     return nav 
 
 def get_index(nav,index_code = '000905.XSHG'):
-    daily_index = jq.get_price(index_code, start_date=np.str(nav.index.values[0])[:10], 
-                               end_date= np.str(nav.index.values[-1])[:10],
-                               frequency='daily',
-                               fields=None, 
-                               skip_paused=False, fq=None, panel=False, fill_paused=False)
-    daily_index = daily_index[['close']]
-    daily_index = daily_index/daily_index[:1].values
-    daily_index = daily_index.reset_index()
-    daily_index = daily_index.sort_values(['index'])
-    daily_index = daily_index.rename(columns = {'close':index_code},inplace = False)
+    daily_index = pd.read_csv('./data/bench_index_nav.csv')
     daily_index['index'] = pd.to_datetime(daily_index['index'],format = '%Y-%m-%d')
+    
+    daily_index = daily_index.loc[(daily_index['index']>=nav.index.values[0])&(daily_index['index']<=nav.index.values[-1]),
+                                  ['index',index_code]]
+    daily_index = daily_index.sort_values(['index'])
+    daily_index[index_code] = daily_index[index_code]/daily_index[index_code][:1].values[0]
     
     nav = nav.reset_index()
     nav['index'] = pd.to_datetime(nav['index'],format = '%Y-%m-%d')
     nav = pd.merge(nav,daily_index,on = ['index'],how = 'left')
     nav = nav.set_index('index')
+    del daily_index
     
     return nav
 
@@ -139,12 +105,14 @@ def get_perf(port_nav,port_name):
     perf['年化波动率-1M'] = [np.std(nav_1[port_name])*(252**0.5)]
     perf['年化夏普-1M'] = [((perf['收益率-1M'].values[0]+1)**(252/21)-1-0.02)/perf['年化波动率-1M'].values[0]]
     perf['最大回撤-1M'] = maxdrop(nav_1[port_name])
+    del nav_1
 
     nav_3 = nav_s[-63:]
     perf['收益率-3M'] = [np.cumprod(1+nav_3[port_name]).values[-1]-1]
     perf['年化波动率-3M'] = [np.std(nav_3[port_name])*(252**0.5)]
     perf['年化夏普-3M'] = [((perf['收益率-3M'].values[0]+1)**(252/63)-1-0.02)/perf['年化波动率-3M'].values[0]]
     perf['最大回撤-3M'] = maxdrop(nav_3[port_name])
+    del nav_3
 
     perf['收益率-'+port_nav.index[0].strftime('%Y/%m/%d')+'至今'] = [np.cumprod(1+nav_s[port_name]).values[-1]-1]
     perf['年化波动率-'+port_nav.index[0].strftime('%Y/%m/%d')+'至今'] = [np.std(nav_s[port_name])*(252**0.5)]
@@ -164,102 +132,85 @@ def get_perf(port_nav,port_name):
                 '年化波动率-1M','年化波动率-3M','年化波动率-'+port_nav.index[0].strftime('%Y/%m/%d')+'至今',
                 '年化夏普-1M','年化夏普-3M','年化夏普-'+port_nav.index[0].strftime('%Y/%m/%d')+'至今',
                 '最大回撤-1M','最大回撤-3M','最大回撤-'+port_nav.index[0].strftime('%Y/%m/%d')+'至今']]
+    del port_nav
+    del nav_s
     
     return perf
 
-
 def get_ETFindex_perf(group,broker_name,mkt,last_rpt_period):
     portfolio = group[(group['券商'] == broker_name)&(group[mkt+'权重']>0)][[mkt + '基金',mkt + '代码',mkt + '权重']]
+    del group
+    
     portfolio[mkt+'代码'] = portfolio[mkt+'代码'].apply(lambda x:np.str(x)+'.OF')
     fund_list = list(portfolio[mkt+'代码'])
-
-    time_end = datetime.now().strftime('%Y-%m-%d')
     
-    index_list = pd.DataFrame(w.wsd(",".join(fund_list),"fund_trackindexcode").Data,
-                              columns = w.wsd(",".join(fund_list),"fund_trackindexcode").Codes)
+    ETF = pd.read_excel('./data/权益类ETF产品列表.xlsx')
+    ETF = ETF[ETF['基金代码'].isin(fund_list)]
+    ETF = ETF[['基金代码','PB(LY)','PE(TTM)','归母净利润同比增长率']]
+    ETF = ETF.rename(columns = {'基金代码':mkt+'代码'},inplace = False)
+    data = pd.merge(ETF,portfolio,on = [mkt+'代码'],how = 'outer')
+    del ETF
+    data = data[[mkt+'基金',mkt+'代码',mkt+'权重','PB(LY)','PE(TTM)','归母净利润同比增长率']]
+    del portfolio
     
-    index_perf = w.wss(",".join(list(index_list.T[0])),"pe_ttm,,pb_lf,yoy_or,yoynetprofit","tradeDate="+time_end+
-                                     ";rptDate="+last_rpt_period)
-    index_perf_table = pd.DataFrame( index_perf.Data,index = index_perf.Fields,columns = index_perf.Codes)
-    index_perf_table = np.round(index_perf_table,2)
-    index_perf_table = index_perf_table.T.reset_index()
-    index_perf_table = index_perf_table.rename(columns = {'PE_TTM':'市盈率PE_TTM',
-                                                          'PB_LF':'市净率PB',
-                                                          'YOY_OR':'营业收入同比增长率（%）',
-                                                          'YOYNETPROFIT':'归母净利润同比增长率（%）'},
-                                             inplace = False)
-    
-    index_list = index_list.T.reset_index()
-    index_list = index_list.rename(columns = {'index':mkt+'代码'},inplace = False)
-    index_list = index_list.rename(columns = {0:'index'},inplace = False)
-    index_list['index'] = index_list['index'].apply(lambda x:x.upper())
-    
-    index_perf_table = pd.merge(index_perf_table,index_list,on = ['index'],how = 'outer')
-    index_perf_table = pd.merge(index_perf_table,portfolio,on = [mkt+'代码'],how = 'outer')
-    index_perf_table = index_perf_table[[mkt+'基金',mkt+'代码',mkt+'权重','市盈率PE_TTM','市净率PB',
-                                         '营业收入同比增长率（%）','归母净利润同比增长率（%）']]
-    return index_perf_table
+    return data
     
 
 def get_holding(group,broker_name,mkt):
     portfolio = group[(group['券商'] == broker_name)&(group[mkt+'权重']>0)][[mkt + '基金',mkt + '代码',mkt + '权重']]
     portfolio[mkt+'代码'] = portfolio[mkt+'代码'].apply(lambda x:np.str(x)+'.OF')
     fund_list = list(portfolio[mkt+'代码'])
-
-    time_start = np.str(group[group['券商'] == broker_name]['时间'].values[0])[:10]
-    time_end = datetime.now().strftime('%Y-%m-%d')
     
-    index_list = pd.DataFrame(w.wsd(",".join(fund_list),"fund_trackindexcode").Data,
-                              columns = w.wsd(",".join(fund_list),"fund_trackindexcode").Codes)
+    ETF = pd.read_excel('./data/权益类ETF产品列表.xlsx')
+    index = ETF.loc[ETF['基金代码'].isin(fund_list),['基金代码','标的指数']]
+    del ETF
     
-    index_consti = pd.DataFrame()
-    for k in index_list.columns:
-        k_consti = pd.DataFrame(w.wset("indexconstituent","date="+time_end+";windcode=" + index_list[k].values[0]).Data).T
-        k_consti.columns = ['date','stk','name','weight','industry']
-        k_consti['weight'] = k_consti['weight']/100*(portfolio.loc[portfolio[mkt+'代码']==k,mkt+'权重'].values[0])
-        
-        index_consti = pd.concat([index_consti,k_consti],sort = True)
-        
-    top_stk = index_consti.groupby(['stk'])['weight'].sum().reset_index()
-    print('组合成分股权重加和等于：',np.round(top_stk['weight'].sum(),4))
-    top_stk = top_stk.sort_values(['weight'],ascending = False)[:20]
+    index_consti = pd.read_excel('./data/指数-持仓权重.xlsx')
+    index_consti = index_consti[index_consti['指数代码'].isin(list(index['标的指数']))]
+    
+    for i in list(index['标的指数']):
+        ETF_code = index.loc[index['标的指数']==i,'基金代码'].values[0]
+        index_consti.loc[index_consti['指数代码']==i,'weight'] = index_consti.loc[index_consti['指数代码']==i,'weight'
+                         ]*portfolio.loc[portfolio[mkt+'代码']==ETF_code,mkt+'权重'].values[0]
+    del index
+    
+    stk = index_consti.groupby(['股票代码'])['weight'].sum().reset_index()
+    del index_consti
+#    print('组合成分股权重加和等于：',np.round(top_stk['weight'].sum(),4))
+    top_stk = stk.sort_values(['weight'],ascending = False)[:20]
     top_stk = top_stk.reset_index(drop = True)
-    print('仓位前20股票权重之和等于',"%.2f%%" % (top_stk['weight'].sum()*100))
-
+#    print('仓位前20股票权重之和等于',"%.2f%%" % (top_stk['weight'].sum()*100))
     
-    stk_l = list(set(top_stk['stk']))
-    df = pd.DataFrame(
-            w.wss(",".join(stk_l), "sec_name,segment_product_item,pct_chg_per",
-                  "rptDate="+np.str(np.float(time_end[:4])-1)[:4]+"1231;order=1;startDate="+
-                  time_start[:4]+time_start[5:7]+time_start[8:10]+
-                  ";endDate="+time_end[:4]+time_end[5:7]+time_end[8:10]).Data).T
-    df.columns = ['股票名称','财报主营构成-项目名称','区间涨跌幅']
-    df['stk'] = stk_l
+    stk_l = list(set(top_stk['股票代码']))
+    df = pd.read_excel('./data/指数全部持仓_标签汇总.xlsx',sheet_name = 'Sheet1')
+    df = df[df['股票代码'].isin(stk_l)]
     
-    top_stk = top_stk.merge(df,on = ['stk'],how = 'outer')    
+    top_stk = top_stk.merge(df[['股票代码','股票名称','财报主营构成-项目名称','区间收益率']],on = ['股票代码'],how = 'outer')    
     top_stk = top_stk.sort_values(['weight'],ascending = False)
     top_stk = top_stk.reset_index(drop = True)
 
     top_stk['weight'] = top_stk['weight'].apply(lambda x:"%.2f%%" % (x * 100))
-    top_stk['区间涨跌幅'] = top_stk['区间涨跌幅'].apply(lambda x:"%.2f%%" % x)
-    top_stk = top_stk.rename(columns = {'stk':'股票代码','weight':'股票权重'
+    top_stk['区间收益率'] = top_stk['区间收益率'].apply(lambda x:"%.2f%%" % x)
+    top_stk = top_stk.rename(columns = {'weight':'股票权重'
                                         ,'财报主营构成-项目名称':'财报主营构成'},inplace = False)
     
-    stk_label = pd.read_excel('./data/指数重仓股票_标签汇总.xlsx',sheet_name = 'Sheet1')
-    stk_label = pd.merge(top_stk[['股票代码','股票权重']],stk_label[['股票代码','一级标签']].drop_duplicates(),
+    label = pd.merge(stk[['股票代码','weight']],df[['股票代码','一级标签','申万行业明细']].drop_duplicates(),
                          on = ['股票代码'],how = 'left')
-    stk_label['股票权重'] = stk_label['股票权重'].apply(lambda x:np.float(x[:-1]))
-    stk_label['股票权重'] = stk_label['股票权重']/stk_label['股票权重'].sum()
-    stk_label = stk_label.groupby(stk_label['一级标签'])['股票权重'].sum().reset_index()
+    del df
+    del stk
+    label = label.rename(columns = {'weight':'股票权重'},inplace = False)
+    label['股票权重'] = label['股票权重']/label['股票权重'].sum()
+    stk_label = label.groupby(label['一级标签'])['股票权重'].sum().reset_index()
     stk_label = stk_label.sort_values(['股票权重'],ascending = False)
     stk_label['股票权重'] = stk_label['股票权重'].apply(lambda x:"%.2f%%" % (x*100))
     stk_label = stk_label.rename(columns = {'股票权重':'标签占比'},inplace = False)
     
-    port_ind = index_consti.groupby(['industry'])['weight'].sum().reset_index()
-    port_ind = port_ind.sort_values(['weight'],ascending = False)
+    port_ind = label.groupby(['申万行业明细'])['股票权重'].sum().reset_index()
+    port_ind = port_ind.sort_values(['股票权重'],ascending = False)
     port_ind = port_ind.reset_index(drop = True)
-    port_ind['weight'] = port_ind['weight'].apply(lambda x:"%.2f%%" % (x * 100))
-    port_ind = port_ind.rename(columns = {'industry':'行业分类','weight':'股票权重'},inplace = False)
+    port_ind['股票权重'] = port_ind['股票权重'].apply(lambda x:"%.2f%%" % (x * 100))
+    
+    del label
     
     return top_stk,port_ind,stk_label
 
